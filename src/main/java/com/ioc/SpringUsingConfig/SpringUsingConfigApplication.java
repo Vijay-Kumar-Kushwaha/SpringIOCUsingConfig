a362bbb30dca577275feb5103f05faee3660206d
Vijay Kumar Kushwaha <vijay77495@gmail.com>
2025-01-17 03:12:51 +0530

    initial commit

---
+++ src/main/java/com/ioc/SpringUsingConfig/SpringUsingConfigApplication.java
@@ -0,0 +1,13 @@
+package com.ioc.SpringUsingConfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUsingConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUsingConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ioc/SpringUsingConfig/SpringUsingConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ioc.SpringUsingConfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUsingConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
